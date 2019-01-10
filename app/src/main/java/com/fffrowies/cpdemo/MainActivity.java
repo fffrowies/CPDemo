3a00f1e926ef254cb9e94ef4791fd4cb877f20ca
fffrowies <f3rnandorowies.saxo@gmail.com>
2019-01-09 21:03:06 -0300

    Updating and deleting records in SQLite Database

--- app/src/main/java/com/fffrowies/cpdemo/MainActivity.java
+++ app/src/main/java/com/fffrowies/cpdemo/MainActivity.java
@@ -1,12 +1,16 @@
 package com.fffrowies.cpdemo;
 
+import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.fffrowies.cpdemo.data.NationContract.NationEntry;
 import com.fffrowies.cpdemo.data.NationDbHelper;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
@@ -76,22 +80,132 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 	private void insert() {
 
+	    String countryName = etCountry.getText().toString();
+	    String continentName = etContinent.getText().toString();
+
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(NationEntry.COLUMN_COUNTRY, countryName);
+        contentValues.put(NationEntry.COLUMN_CONTINENT, continentName);
+
+        long rowId = database.insert(NationEntry.TABLE_NAME, null, contentValues);
+        Log.i(TAG, "Items inserted in table with row id: " + rowId);
 	}
 
 	private void update() {
 
+	    String newContinent = etNewContinent.getText().toString();
+	    String whereCountry = etWhereToUpdate.getText().toString();
+
+	    ContentValues contentValues = new ContentValues();
+	    contentValues.put(NationEntry.COLUMN_CONTINENT, newContinent);
+
+	    String selection = NationEntry.COLUMN_COUNTRY + " = ?";
+	    String[] selectionArgs = { whereCountry };              // WHERE country = Japan (i.e.)
+
+	    int rowsUpdated = database.update(
+	            NationEntry.TABLE_NAME,
+                contentValues,
+                selection,
+                selectionArgs
+        );
+	    Log.i(TAG, "Number of rows updated: " + rowsUpdated);
 	}
 
 	private void delete() {
 
+	    String countryName = etWhereToDelete.getText().toString();
+
+	    String selection = NationEntry.COLUMN_COUNTRY + " = ? ";
+	    String[] selectionArgs = { countryName };               // WHERE country = "Japan" (i.e.)
+
+	    int rowsDeleted = database.delete(
+	            NationEntry.TABLE_NAME,
+                selection,
+                selectionArgs
+        );
+	    Log.i(TAG, "Number of rows deleted: " + rowsDeleted);
 	}
 
 	private void queryRowById() {
 
+	    String rowId = etQueryRowById.getText().toString();
+
+        String[] projection = {
+                NationEntry._ID,
+                NationEntry.COLUMN_COUNTRY,
+                NationEntry.COLUMN_CONTINENT
+        };
+
+        // Filter results. Make these null if you want to query all rows
+        String selection = NationEntry._ID + " = ? ";   // _id = ?
+        String[] selectionArgs = { rowId };             // replace '?' by rowId in runtime
+
+        String sortOrder = null;  // ascending or descending...
+
+        Cursor cursor = database.query(
+                NationEntry.TABLE_NAME,         // the table name
+                projection,                     // the columns to return
+                selection,                      // Selection: WHERE clause OR the condition
+                selectionArgs,                  // Selection arguments for the WHERE clause
+                null,                   // don't group the rows
+                null,                    // don't filter by row groups
+                sortOrder
+        );                     // the sort order
+
+        if (cursor != null && cursor.moveToNext()) {
+
+            String str = "";
+
+            String[] columns = cursor.getColumnNames();
+            for (String column : columns) {
+                str += "\t" + cursor.getString(cursor.getColumnIndex(column));
+            }
+            str += "\n";
+
+            cursor.close();
+            Log.i(TAG, str);
+        }
 	}
 
 	private void queryAndDisplayAll() {
 
+	    String[] projection = {
+	            NationEntry._ID,
+	            NationEntry.COLUMN_COUNTRY,
+                NationEntry.COLUMN_CONTINENT
+        };
+
+	    // Filter results. Make these null if you want to query all rows
+	    String selection = null;
+	    String[] selectionArgs = null;
+
+	    String sortOrder = null;  // ascending or descending...
+
+	    Cursor cursor = database.query(
+	            NationEntry.TABLE_NAME,         // the table name
+                projection,                     // the columns to return
+                selection,                      // Selection: WHERE clause OR the condition
+                selectionArgs,                  // Selection arguments for the WHERE clause
+                null,                   // don't group the rows
+                null,                    // don't filter by row groups
+                sortOrder
+        );                     // the sort order
+
+        if (cursor != null) {
+
+            String str = "";
+            while (cursor.moveToNext()) {       // Cursor iterates through all rows
+
+                String[] columns = cursor.getColumnNames();
+                for (String column : columns) {
+                    str += "\t" + cursor.getString(cursor.getColumnIndex(column));
+                }
+                str += "\n";
+            }
+
+            cursor.close();
+            Log.i(TAG, str);
+        }
 	}
 
     @Override
